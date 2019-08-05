5c850d8980d59a3e00f38f5a64327715ac35af89
sebas07casti <sebas07casti@gmail.com>
2019-08-05 10:19:31 -0500

    Favicon

--- layout/archivosheader.php
+++ layout/archivosheader.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/all.min.css">
 <link rel="stylesheet" href="css/style.css">
