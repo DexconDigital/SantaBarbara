eb22bdd9725e807e513c8a70dfe48aedfb3a77dd
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-05-25 10:28:51 -0500

    ajustes a pdo

--- santaBarbara-admin/admin/conexion.php
+++ santaBarbara-admin/admin/conexion.php
@@ -2,8 +2,17 @@
 
 function Conect()
 {
-    $echo = mysqli_connect("localhost","root","","dexcondigital_noticias_inmueble");
-    return $echo;
+    /*$echo = mysqli_connect("localhost","root","","dexcondigital_noticias_inmueble");
+    return $echo;*/
+    try {
+        $echo = new PDO('mysql:host=localhost;port=3306;dbname=dexcondigital_noticias_inmueble;', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET NAMES utf8"));
+        $echo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $echo->exec("set names utf8");
+        return $echo;
+
+    } catch ( Exception $e ) {
+        die( 'Error:' . $e->getMessage() );
+    }
 }
 
 ?>
