b8c61d49a794dc897f4551bc34866021daf51755
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-05-25 10:55:10 -0500

    Ajustes de pdo

--- santaBarbara-admin/admin/layout/layout.php
+++ santaBarbara-admin/admin/layout/layout.php
@@ -1,10 +1,11 @@
 <?php
 $user = $_SESSION["usuarioactual"];
 $con = Conect();
-$qry = "SELECT * FROM usuarios where usuario ='$user'";
-$sql = mysqli_query($con, $qry);
-$usuario =  mysqli_fetch_array($sql);
-$imagen_inicio = $usuario[7];
+$qry = "SELECT * FROM usuarios where usuario = '{$user->usuario}'";
+$result = $con->prepare( $qry );
+$result->execute();
+$usuario = $result->fetch( PDO::FETCH_OBJ );
+$imagen_inicio = $usuario->imagen;
 ?>
 <!DOCTYPE html>
 <html lang="es">
